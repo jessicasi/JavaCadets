2cf320e7954719ac9e36de67bb814e85d7dde177
StevenWeber <>
2018-10-08 22:52:21 -0700

    10-8-18 Added all locations

---
+++ cityOfAaron/CityofAaron/src/byui/cit260/JavaCadets/model/ItemType.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.JavaCadets.model;
+
+/**
+ *
+ * @author Steven
+ */
+public enum ItemType {
+    
+    Animal(Math.random()*((1-20)+1))+20, Good)
+
+    
+    private int age;
+    private Condition condition;
+   
+ItemType(String age, String condition){
+    this.animal = animal;
+    this.condition = condition;
+    
+}
+    
+}

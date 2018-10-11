69985e3712fa05cfb34a624f146964e442b629f3
skylerfoxx <skylerfoxx@imac02-s.local>
2018-10-11 04:53:50 -0700

    10/11/2018 - Initial Commit to repository by Skyler

--- cityOfAaron/CityofAaron/src/byui/cit260/JavaCadets/model/Provision.java
+++ cityOfAaron/CityofAaron/src/byui/cit260/JavaCadets/model/Provision.java
@@ -5,10 +5,67 @@
  */
 package byui.cit260.JavaCadets.model;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 /**
  *
- * @author Steven
+ * @author Skyler
  */
-public class Provision {
+public class Provision implements Serializable {
+    
+    private String name;
+    private boolean perishable;
+
+    public Provision() {
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isPerishable() {
+        return perishable;
+    }
+
+    public void setPerishable(boolean perishable) {
+        this.perishable = perishable;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 79 * hash + Objects.hashCode(this.name);
+        hash = 79 * hash + (this.perishable ? 1 : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Provision other = (Provision) obj;
+        if (this.perishable != other.perishable) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
     
 }

5553ae8d9b48f01c08d5194e10eaca187d57b9ad
Steven <Steven@DESKTOP-KUP6N42.rcore>
2018-10-10 20:59:40 -0700

    10-10-18 Added Point,Scene, Question.

--- cityOfAaron/CityofAaron/src/byui/cit260/JavaCadets/model/Scene.java
+++ cityOfAaron/CityofAaron/src/byui/cit260/JavaCadets/model/Scene.java
@@ -6,11 +6,63 @@
 package byui.cit260.JavaCadets.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  *
  * @author Steven
  */
 public class Scene implements Serializable{
+    
+    private String name;
+    private String description;
+    private Question question;
+    private InventoryItem item;
+
+    public Scene() {
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 13 * hash + Objects.hashCode(this.name);
+        hash = 13 * hash + Objects.hashCode(this.description);
+        hash = 13 * hash + Objects.hashCode(this.question);
+        hash = 13 * hash + Objects.hashCode(this.item);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Scene other = (Scene) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (!Objects.equals(this.question, other.question)) {
+            return false;
+        }
+        if (!Objects.equals(this.item, other.item)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Scene{" + "name=" + name + ", description=" + description + ", question=" + question + ", item=" + item + '}';
+    }
+    
     
 }

7b768fab3f97023b8b9c7275b20061818100f1f0
skylerfoxx <skylerfoxx@imac02-s.local>
2018-10-22 20:56:23 -0700

    Merge origin/master
    Conflicts:
    	cityOfAaron/CityofAaron/src/byui/cit260/JavaCadets/model/Animal.java
    	cityOfAaron/CityofAaron/src/byui/cit260/JavaCadets/model/Map.java
    	cityOfAaron/CityofAaron/src/byui/cit260/JavaCadets/model/Provision.java

---
+++ cityOfAaron/CityofAaron/src/byui/cit260/JavaCadets/control/PlantCrops.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.JavaCadets.control;
+
+/**
+ *
+ * @author skylerfoxx
+ */
+public class PlantCrops {
+    
+    public int plantCrops(int acres, int wheat, int userAcres) {
+    
+        if (userAcres <= 0) {
+            return -3; 
+        }
+        if (userAcres > acres) {
+            return -1;
+        }
+        
+        int wheatNeeded = userAcres/2 ;
+        
+        if (wheatNeeded == 0) {
+            wheatNeeded += 1;
+        }
+        if (wheatNeeded > wheat) {
+            return -2;
+        }
+        wheat -= wheatNeeded;
+        
+        return wheat;
+        
+    
+    }
+    
+}

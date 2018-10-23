236e90cd5b47c24226031857c6f2e582dd540916
skylerfoxx <skylerfoxx@imac02-s.local>
2018-10-22 21:21:00 -0700

    Merge origin/master
    Conflicts:
    	cityOfAaron/CityofAaron/src/byui/cit260/JavaCadets/model/Animal.java
    	cityOfAaron/CityofAaron/src/byui/cit260/JavaCadets/model/Map.java
    	cityOfAaron/CityofAaron/src/byui/cit260/JavaCadets/model/Provision.java

---
+++ cityOfAaron/CityofAaron/test/byui/cit260/JavaCadets/control/PlantCropsTest.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.JavaCadets.control;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author skylerfoxx
+ */
+public class PlantCropsTest {
+    
+    public PlantCropsTest() {
+    }
+
+    /**
+     * Test of plantCrops method, of class PlantCrops.
+     */
+    @Test
+    
+    public void testPlantCrops() {
+        System.out.println("Test Case 1");
+        int acres = 5;
+        int wheat = 10;
+        int userAcres = 2;
+        PlantCrops instance = new PlantCrops();
+        int expResult = 9;
+        int result = instance.plantCrops(acres, wheat, userAcres);
+        assertEquals(expResult, result);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        // fail("The test case is a prototype.");
+    
+        System.out.println("Test Case 2");
+        acres = 0;
+        wheat = 10;
+        userAcres = 1;
+        // PlantCrops instance = new PlantCrops();
+        expResult = -1;
+        result = instance.plantCrops(acres, wheat, userAcres);
+        assertEquals(expResult, result);
+        assertEquals(expResult, result);
+        
+        System.out.println("Test Case 3");
+        acres = 10;
+        wheat = 0;
+        userAcres = 2;
+        // PlantCrops instance = new PlantCrops();
+        expResult = -2;
+        result = instance.plantCrops(acres, wheat, userAcres);
+        assertEquals(expResult, result);
+        assertEquals(expResult, result);
+        
+        System.out.println("Test Case 4");
+        acres = 10;
+        wheat = 5;
+        userAcres = 0;
+        // PlantCrops instance = new PlantCrops();
+        expResult = -3;
+        result = instance.plantCrops(acres, wheat, userAcres);
+        assertEquals(expResult, result);
+        assertEquals(expResult, result);
+        
+        System.out.println("Test Case 5");
+        acres = 1;
+        wheat = 10;
+        userAcres = 1;
+        // PlantCrops instance = new PlantCrops();
+        expResult = 9;
+        result = instance.plantCrops(acres, wheat, userAcres);
+        assertEquals(expResult, result);
+        assertEquals(expResult, result);
+        
+        System.out.println("Test Case 6");
+        acres = 10;
+        wheat = 1;
+        userAcres = 2;
+        // PlantCrops instance = new PlantCrops();
+        expResult = 0;
+        result = instance.plantCrops(acres, wheat, userAcres);
+        assertEquals(expResult, result);
+        assertEquals(expResult, result);
+        
+        System.out.println("Test Case 7");
+        acres = 10;
+        wheat = 5;
+        userAcres = 1;
+        // PlantCrops instance = new PlantCrops();
+        expResult = 4;
+        result = instance.plantCrops(acres, wheat, userAcres);
+        assertEquals(expResult, result);
+        assertEquals(expResult, result);
+        
+        // TODO review the generated test code and remove the default call to fail.
+        // fail("The test case is a prototype.");
+    
+    }
+        
+}

cb180960019ea21b805852aaf37a6accd3a500bc
skylerfoxx <skylerfoxx@imac02-s.local>
2018-10-27 17:36:32 -0700

    Merge origin/master
    Conflicts:
    	cityOfAaron/CityofAaron/src/byui/cit260/JavaCadets/model/Animal.java
    	cityOfAaron/CityofAaron/src/byui/cit260/JavaCadets/model/Map.java
    	cityOfAaron/CityofAaron/src/byui/cit260/JavaCadets/model/Provision.java

--- cityOfAaron/CityofAaron/test/byui/cit260/JavaCadets/control/BuyLandTest.java
+++ cityOfAaron/CityofAaron/test/byui/cit260/JavaCadets/control/BuyLandTest.java
@@ -23,20 +23,20 @@ public class BuyLandTest {
     @Test
     public void testBuyLand() {
         System.out.println("Test Case 1");
-        int userAcres = 1;
-        int wheat = 20;
-        int acresBought = 1;
-        int landPrice = 15;
+        int userAcres = 2;
+        int wheat = 15;
+        int acresBought = 2;
+        int landPrice = 5;
         BuyLand instance = new BuyLand();
         int expResult = 5;
         int result = instance.buyLand(userAcres, wheat, acresBought, landPrice);
         assertEquals(expResult, result);
         
         System.out.println("Test Case 2");
-        userAcres = 3;
+        userAcres = 1;
         wheat = 5;
         acresBought = 2;
-        landPrice = 5;
+        landPrice = 15;
         // BuyLand instance = new BuyLand();
         expResult = -1;
         result = instance.buyLand(userAcres, wheat, acresBought, landPrice);
@@ -44,21 +44,51 @@ public class BuyLandTest {
         
         System.out.println("Test Case 3");
         userAcres = 3;
-        wheat = 5;
+        wheat = 30;
         acresBought = 0;
-        landPrice = 15;
+        landPrice = 10;
         // BuyLand instance = new BuyLand();
         expResult = -2;
         result = instance.buyLand(userAcres, wheat, acresBought, landPrice);
         assertEquals(expResult, result);
         
         System.out.println("Test Case 4");
-        userAcres = 0;
-        wheat = 35;
+        userAcres = 4;
+        wheat = 40;
         acresBought = 2;
-        landPrice = 15;
+        landPrice = 20;
         // BuyLand instance = new BuyLand();
-        expResult = 5;
+        expResult = 0;
+        result = instance.buyLand(userAcres, wheat, acresBought, landPrice);
+        assertEquals(expResult, result);
+        
+        System.out.println("Test Case 5");
+        userAcres = 0;
+        wheat = 30;
+        acresBought = 2;
+        landPrice = 9;
+        // BuyLand instance = new BuyLand();
+        expResult = 12;
+        result = instance.buyLand(userAcres, wheat, acresBought, landPrice);
+        assertEquals(expResult, result);
+        
+        System.out.println("Test Case 6");
+        userAcres = 10;
+        wheat = 5;
+        acresBought = 4;
+        landPrice = 1;
+        // BuyLand instance = new BuyLand();
+        expResult = 1;
+        result = instance.buyLand(userAcres, wheat, acresBought, landPrice);
+        assertEquals(expResult, result);
+        
+        System.out.println("Test Case 7");
+        userAcres = 3;
+        wheat = 25;
+        acresBought = 3;
+        landPrice = 7;
+        // BuyLand instance = new BuyLand();
+        expResult = 4;
         result = instance.buyLand(userAcres, wheat, acresBought, landPrice);
         assertEquals(expResult, result);
         

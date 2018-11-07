5bc893ced06ee9fdbd986f2d80b2977513c0fd90
Jessica Ingebrigtsen <aron.jess@gmail.com>
2018-11-06 22:51:23 -0800

    11-07-18 Save Current Annual Report View

---
+++ cityOfAaron/CityofAaron/src/byui/cit260/JavaCadets/view/CurrentAnnualReportView.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.JavaCadets.view;
+
+/**
+ *
+ * @author Jessica
+ */
+public class CurrentAnnualReportView {
+   public void displayCurrentAnnualReportView() {
+       //eventually hook up to variables
+       System.out.println("Year Number: ");
+       
+       System.out.println("# of people who starved: ");
+       
+       System.out.println("# of people who moved in: ");
+       
+       System.out.println("Current Population: ");
+       
+       System.out.println("# of acres of crop land owned: ");
+       
+       System.out.println("# of bushels of wheat paid in offerings: ");
+       
+       System.out.println("# of bushels of wheat eaten by rats: ");
+       
+       System.out.println("# of bushles of wheat in storehouse: ");
+    }
+}

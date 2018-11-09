6f67014303d20ed012a4dda1721bffa197175760
skylerfoxx <skylerfoxx@imac02-s.local>
2018-11-08 17:45:05 -0700

    Merge origin/master
    Conflicts:
    	cityOfAaron/CityofAaron/src/byui/cit260/JavaCadets/model/Animal.java
    	cityOfAaron/CityofAaron/src/byui/cit260/JavaCadets/model/Map.java
    	cityOfAaron/CityofAaron/src/byui/cit260/JavaCadets/model/Provision.java

--- cityOfAaron/CityofAaron/src/byui/cit260/JavaCadets/control/BuyLand.java
+++ cityOfAaron/CityofAaron/src/byui/cit260/JavaCadets/control/BuyLand.java
@@ -5,29 +5,58 @@
  */
 package byui.cit260.JavaCadets.control;
 
+import java.util.Scanner;
+
 /**
  *
  * @author skylerfoxx
  */
 public class BuyLand {
     
+    public void displayBuyLand() {
+    }
+    
     public int buyLand (int userAcres, int wheat, int acresBought, int landPrice){
         
-        //Price of land is multiplied by land selected
-        int totalPrice = landPrice * acresBought;
+        boolean endOfView = false;
         
-        if (wheat < totalPrice) {
-            return -1;
+        //Sets price for each land
+        landPrice = 10;
+        
+        System.out.println("1 Land costs" + "" + landPrice);
+        System.out.println("How much land do you want to buy?");
+        System.out.println();
+        
+        do {
+            //Get User Input
+            Scanner inFile;
+            inFile = new Scanner(System.in);
+            acresBought = inFile.nextInt();
+
+            //TODO: Change this to call getWheatAvailable() from the Game class
+            int wheatAvailable = 30;
+            //Total Price is multiplied by land selection
+            int totalPrice = landPrice * acresBought;
+            System.out.println();
+            System.out.println("The total price for " + acresBought + " land is " + totalPrice);
+
+            if (wheat < totalPrice) {
+                System.out.println("You don't have enough wheat to purchase this land.");
+            }
+            if (acresBought <= 0) {
+                return -2;
+            }
+            
+            else {
+                userAcres += acresBought;
+                wheat -= totalPrice;
+
+            }
         }
-        if (acresBought <= 0) {
-            return -2;
-        }
-       
-        userAcres += acresBought;
         
-        wheat -= totalPrice;
+        while (endOfView == false);
         
-        return wheat;
+        return wheat & acresBought;
             
     }
         

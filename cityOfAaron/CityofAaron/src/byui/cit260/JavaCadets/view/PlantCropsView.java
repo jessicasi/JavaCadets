2684e726704ea8bbd4ecc81e6a7126acca18a0fe
skylerfoxx <skylerfoxx@imac02-s.local>
2018-11-11 02:21:09 -0700

    Merge origin/master
    Conflicts:
    	cityOfAaron/CityofAaron/src/byui/cit260/JavaCadets/model/Animal.java
    	cityOfAaron/CityofAaron/src/byui/cit260/JavaCadets/model/Map.java
    	cityOfAaron/CityofAaron/src/byui/cit260/JavaCadets/model/Provision.java

---
+++ cityOfAaron/CityofAaron/src/byui/cit260/JavaCadets/view/PlantCropsView.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.JavaCadets.view;
+
+import byui.cit260.JavaCadets.control.GameControl;
+import byui.cit260.JavaCadets.control.PlantCrops;
+import byui.cit260.JavaCadets.model.Player;
+import java.util.Scanner;
+
+/**
+ *
+ * @author skylerfoxx
+ */
+public class PlantCropsView {
+
+    public void displayPlantCropsView() {
+        boolean endOfView = false;
+        do {
+            String[] inputs = getInputs();
+            //no inputs were entered OR the first input is Q
+            if (inputs.length < 1 || inputs[0].equals("Q")) {
+                System.out.println("Progam end");
+                endOfView = true;
+            }
+            endOfView = doAction(inputs);
+        } while (endOfView != true);
+
+    }
+
+    private String[] getInputs() {
+        PlantCrops buyLand = new PlantCrops();
+
+        String[] inputs = new String[1];
+        
+        System.out.println(" ********************** ");
+        System.out.println(" ***  Plant Crops  *** ");
+        System.out.println(" ********************** ");
+        System.out.println();
+        System.out.println("P - Plant Crops");
+        System.out.println("Q - Quit Buying Land");
+
+         boolean valid = false;
+
+        while (valid == false) {
+            System.out.println("Make a selection from the menu");
+            //Get the value entered from the keyboard
+            Scanner inFile;
+            inFile = new Scanner(System.in);
+            inputs[0] = inFile.nextLine();
+            // Trim off leading and trailing blanks from the value
+            inputs[0] = inputs[0].trim();
+            //Make sure user entered a value
+
+            if (inputs[0].equals("")) {
+                System.out.println("Please enter a non-blank value");
+                continue;
+            }
+            valid = true;
+        }
+
+        return inputs;
+    }
+    
+    private boolean doAction(String[] inputs) {
+        String menuItem = inputs[0].toUpperCase();
+
+        switch (menuItem) {
+
+            case "B": {
+                buyLand();
+            }
+                return true;
+
+            case "Q":
+                return true;
+
+            default: {
+                System.out.println("Invalid menu item");
+            }
+            break;
+        }
+        return false;
+    }
+
+
+    private void buyLand() {
+
+        PlantCrops PlantCrops = new PlantCrops();
+        PlantCrops.plantCrops(0, 0, 0);
+    }
+}

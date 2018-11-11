6b4b47276f4d88cde9c2b6be4e7b1c6135b8a68e
Jessica Ingebrigtsen <aron.jess@gmail.com>
2018-11-10 23:12:46 -0800

    11-08-18 Updates to BuyLand

--- cityOfAaron/CityofAaron/src/byui/cit260/JavaCadets/control/BuyLand.java
+++ cityOfAaron/CityofAaron/src/byui/cit260/JavaCadets/control/BuyLand.java
@@ -21,24 +21,27 @@ public class BuyLand {
         if (price < 17 || price > 27) {
             return -3;
         }
-       System.out.println("The cost of 1 acre of land right now is" + price);
+       System.out.println("The cost of 1 acre of land right now is " + price + " wheat.");
+       System.out.println();
       
         boolean enoughWheat = false;
         boolean enoughPeople = false;   
         int landToBuy = 0;
+        
         do{   
-        //Get User Input
+            //TODO: Change this to call getWheatAvailable() from the Game class
             int wheatAvailable = 300;
-            System.out.println("You have " + wheatAvailable + " much wheat to buy land with.");
+            System.out.println("You have " + wheatAvailable + " wheat to buy land with.");
             System.out.println("How many acres do you want to buy?");
             Scanner inFile;
             inFile = new Scanner(System.in);
             landToBuy = inFile.nextInt();
 
-            //TODO: Change this to call getWheatAvailable() from the Game class
+            
             int wheatNeeded = price * landToBuy;
             System.out.println();
             System.out.println("It will take " + wheatNeeded + " wheat to buy that much land.");
+            System.out.println();
 
             if (wheatAvailable < wheatNeeded) {
                 System.out.println("You don't have enough wheat to buy that much land. Enter a lower number");
@@ -61,17 +64,21 @@ public class BuyLand {
     private boolean calculatePeople(int landToBuy) {
                 
         //TODO: get number of people from Game class
-        int people = 75;
+        int people = 60;
         //TODO: get land already owned from Game Class
         int land = 20;
         
         int peopleLeft = people - (land/10);
         int peopleNeeded = landToBuy / 10;
         
-        if (peopleLeft > peopleNeeded) {
+        if (peopleLeft < peopleNeeded) {
             System.out.println("You don't have enough people to tend the land. Enter a lower number");
             return false;
         }
+        else{
+            System.out.println("Congratulations, you just bought " + landToBuy + " acres of land!");
+        }
+        
         return true;
         
     }

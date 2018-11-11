7fd2bfd12d0128c4d3358236a8eb8d7b5eed1a38
skylerfoxx <skylerfoxx@imac02-s.local>
2018-11-11 02:13:57 -0700

    Merge origin/master
    Conflicts:
    	cityOfAaron/CityofAaron/src/byui/cit260/JavaCadets/model/Animal.java
    	cityOfAaron/CityofAaron/src/byui/cit260/JavaCadets/model/Map.java
    	cityOfAaron/CityofAaron/src/byui/cit260/JavaCadets/model/Provision.java

---
+++ cityOfAaron/CityofAaron/src/byui/cit260/JavaCadets/control/FeedThePeople.java
@@ -0,0 +1,77 @@
+    /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.JavaCadets.control;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author skylerfoxx
+ */
+public class FeedThePeople {
+    
+    public int FeedThePeople (){
+        
+        //TODO: get number of people from Game class
+        int people = 75;
+        //TODO: get land already owned from Game Class
+        int land = 20;
+        //TODO: get wheat already owned from Game Class
+        int userWheat = 200;
+        
+        //Calculate wheat needed to feed everyone
+        int wheatNeeded = people * 2;
+        
+        //Set number of people fed
+        int peopleFed = 0;
+        
+        System.out.println("Your population consists of " + people + " across " + land + " acre(s) of land.");
+        System.out.println("It takes 2 bushels of wheat to feed each person.");
+        System.out.println("You have " + userWheat + " wheat remaining.");
+        System.out.println("To feed everyone will require " + wheatNeeded + " wheat.");
+        System.out.println();
+        System.out.println("How much wheat will you give to the people?");
+      
+        boolean enoughWheat = false;
+        int wheatFeed = 0;
+        do{   
+        //Get User Input
+            System.out.println();
+            Scanner inFile;
+            inFile = new Scanner(System.in);
+            wheatFeed = inFile.nextInt();
+
+            if (wheatFeed <= 0) {
+                System.out.println();
+                System.out.println("You neglected to feed your people!");
+                System.out.println();
+                enoughWheat = true;
+                continue;
+            }
+            
+            if (userWheat < wheatNeeded) {
+                System.out.println("Unfortunately, you don't have that much wheat. Enter a lower number.");
+                System.out.println();
+                
+            } else {
+                userWheat -= wheatFeed;
+                System.out.println("You gave " + wheatFeed + " wheat to the people. You now have " + userWheat + " wheat remaining.");
+                
+                peopleFed = wheatFeed / 2;
+                
+                System.out.println("You fed " + peopleFed + " out of the " + people + " people." );
+                System.out.println();
+                
+                enoughWheat = true;
+            }
+               //TODO: Call setWheatAvailable() from the Game class
+            } while (enoughWheat == false);
+
+        //Should never reach this point
+        return wheatFeed;
+    }
+        
+}
